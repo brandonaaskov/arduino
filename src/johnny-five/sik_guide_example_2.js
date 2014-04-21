37868bfdc684c788fa884d733edf9cf1b0370c74
Brandon Aaskov <baaskov@fullscreen.net>
2014-04-20 22:28:47 -0700

    Finally. A solution that I'm content with. The trick was that calling strobe without first turning it off basically made it look like it was constantly on. Now, with this fluctuation checking and gentle throttling of the function that gets called when it changes enough, this works about exactly the same as the SIK example.

--- src/johnny-five/sik_guide_example_2.js
+++ src/johnny-five/sik_guide_example_2.js
@@ -1,24 +1,26 @@
 var five = require('johnny-five'),
+  _ = require('lodash'),
   board = board = new five.Board();
 
 
 board.on("ready", function() {
-  var myAnalogPin = new five.Pin('A0');
   var myLed = new five.Led(13);
   var currentVoltage = 0;
-  var board = this;
 
-  board.analogRead(0, function(voltage) {
-    //delay and turn on
-    board.wait(voltage, function () {
-      console.log('on');
-      board.digitalWrite(13, 1);
+  var flasher = _.throttle(function () {
+    console.log('strobing', currentVoltage);
+    myLed.stop().off();
+    myLed.strobe(currentVoltage);
+  }, 500);
 
-      //delay and turn off
-      board.wait(voltage, function () {
-        console.log('off');
-        board.digitalWrite(13, 0);
-      });
-    });
+  this.analogRead(0, function (voltage) {
+    if (currentVoltage !== voltage) {
+      var drop = currentVoltage - voltage > 1
+      var spike = voltage - currentVoltage > 1;
+
+      if (drop || spike) flasher();
+
+      currentVoltage = voltage;
+    }
   });
 });
