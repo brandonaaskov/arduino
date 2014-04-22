c454d6100e80f057f75897ba4f9b20d26961d1d6
Brandon Aaskov <baaskov@fullscreen.net>
2014-04-21 18:04:52 -0700

    Changed the returns to make it readable on github.

--- src/johnny-five/sik_guide_example_2.js
+++ src/johnny-five/sik_guide_example_2.js
@@ -8,7 +8,10 @@ board.on("ready", function() {
   var currentVoltage = 0;
 
   /*
-  Since the analogRead() method gets called several times a second, we throttle  it here so that it doesn't attempt to turn off and then strobe our led too much. We're only throttling to 500ms so that there also isn't a noticeable delay in the real world.
+  Since the analogRead() method gets called several times a second, we throttle
+  it here so that it doesn't attempt to turn off and then strobe our led too
+  much. We're only throttling to 500ms so that there also isn't a noticeable
+  delay in the real world.
   */
   var flasher = _.throttle(function () {
     console.log('strobing', currentVoltage);
@@ -18,7 +21,11 @@ board.on("ready", function() {
 
   this.analogRead(0, function (voltage) {
     if (currentVoltage !== voltage) {
-      // depending on the position of the pot, it can "flicker" between two values so these two variables give us a threshold of +/- 1. If the change is greater than that, then we fun the flasher() method.
+      /*
+      depending on the position of the pot, it can "flicker" between two values
+      so these two variables give us a threshold of +/- 1. If the change is
+      greater than that, then we fun the flasher() method.
+      */
       var drop = currentVoltage - voltage > 1
       var spike = voltage - currentVoltage > 1;
 
